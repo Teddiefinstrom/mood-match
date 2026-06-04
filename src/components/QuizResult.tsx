fbe2f3f2d2d1b59caa42955b220955873e1ef7b8
Teddie Finstrom <teddie.finstrom@outlook.com>
2026-06-04 19:06:07 +0200

    add album scoring, tie handling and random winner selection

--- src/components/QuizResult.tsx
+++ src/components/QuizResult.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { albums } from "../data/albums";
 import type { AlbumTag } from "../types/Album";
 
@@ -5,21 +6,53 @@ interface QuizResultProps {
   selectedTags: AlbumTag[];
 }
 
-const QuizResult = ({selectedTags}: QuizResultProps) => {
-    
+const QuizResult = ({ selectedTags }: QuizResultProps) => {
+  console.log(selectedTags);
 
-console.log(selectedTags);
+  const albumResult = albums.map((a) => {
+    const matchingTags = a.albumTags.filter((tag) =>
+      selectedTags.includes(tag),
+    );
+
+    const score = matchingTags.length;
+
+    return {
+      album: a,
+      score: score,
+    };
+  });
+
+      console.log(albumResult);
+
+  const winner = albumResult.reduce((bestSoFar, currentItem) => {
+    if (bestSoFar.score > currentItem.score) {
+      return bestSoFar;
+    } else {
+      return currentItem;
+    }
+  });
+
+  const highestScoredAlbums = albumResult.filter((highest) => (highest.score === winner.score));
+
+const finalWinner = useMemo(() => {
+  // eslint-disable-next-line react-hooks/purity
+  const random = Math.floor(Math.random() * highestScoredAlbums.length);
+  return highestScoredAlbums[random];
+}, [highestScoredAlbums]);
+
+  console.log(winner.score);
+  
 
   return (
     <>
       <div className="quiz-container">
         <div className="container">
           <p>Your result:</p>
-          <h2>{albums[0].title}</h2>
-          <p>{albums[0].year}</p>
-          <p>{albums[0].artist}</p>
-          <img src={albums[0].coverUrl} alt="" />
-          <p>{albums[0].description}</p>
+          <h2>{finalWinner.album.title}</h2>
+          <p>{finalWinner.album.year}</p>
+          <p>{finalWinner.album.artist}</p>
+          <img src={finalWinner.album.coverUrl} alt={`${finalWinner.album.title} album cover`} />
+          <p>{finalWinner.album.description}</p>
         </div>
       </div>
     </>
